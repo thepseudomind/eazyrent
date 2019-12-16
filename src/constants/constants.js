65acd251a60f90c0c561e69d2388866c302ac8d5
oderindeolayemi <oderinde.yemi@gmail.com>
2019-12-16 18:21:05 +0100

    fixed edit bank feature

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const PAGEROUTE = 'PAGEROUTE';
 export const ADDTOBANK = 'ADDTOBANK';
 export const EDITBANK = 'EDITBANK';
@@ -7,6 +6,3 @@ export const DELETEBANK = 'DELETEBANK';
 export const CHANGEFORM = 'CHANGEFORM';
 export const RESETALERTBOX = 'RESETALERTBOX';
 export const RETRIEVESTATE = 'RETRIEVESTATE'; 
-=======
-export const PAGEROUTE = 'PAGEROUTE';
->>>>>>> b1b957c725a0de1dfa657c0b6f81e51305a992f7
